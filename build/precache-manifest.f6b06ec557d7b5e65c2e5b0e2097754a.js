63fbe418af334d7958833a312c386ec3e84e4b45
henry0928 <a0975938830@gmail.com>
2023-03-02 18:54:51 +0800

    init

---
+++ build/precache-manifest.f6b06ec557d7b5e65c2e5b0e2097754a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd3ae08fa9714a0e67cfd658367cb85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53f581314d096f156c2b",
+    "url": "/static/js/2.48e09751.chunk.js"
+  },
+  {
+    "revision": "3b897328cbab03023c956119c3982cbf",
+    "url": "/static/js/2.48e09751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afeafc106698900b60b",
+    "url": "/static/js/main.0e6e3b0c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0e6e3b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90eb002640db4b519802",
+    "url": "/static/js/runtime-main.0945f2ee.js"
+  }
+]);
