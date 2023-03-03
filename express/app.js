129a606e72dfeebb3afb45edc0d739b0a214ffe3
B10732009 <yuchen4667@yahoo.com.tw>
2023-03-03 16:48:50 +0800

    add script for building and integrating react app

--- express/app.js
+++ express/app.js
@@ -12,7 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static('build'));
+app.use(express.static('react_build')); // set the path of react app package 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(logger('dev'));
@@ -25,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
