fae301a72d6cf506c973bfd08ba69fc69f1bcb9d
B10732009 <yuchen4667@yahoo.com.tw>
2023-03-30 14:39:08 +0800

    Handle SignMessage

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -1,58 +1,42 @@
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-
-// const [address, setAddress] = useState("");
-// const [balance, setBalance] = useState("");
-// const [network, setNetwork] = useState("");
-// const [isConnected, setIsConnected] = useState(false);
-// const [signedMessage, setSignedMessage] = useState("");
-// const [provider, setProvider] = useState("");
-// const [walletEmail, setWalletEmail] = useState("");
-
+import { ethers } from "ethers";
 
 const Web3 = require("web3");
 let CerthisWalletLib = require("@certhis/certhis-wallet");
-
-let CerthisWallet = CerthisWalletLib.init(
-    Web3,
-    CoinbaseWalletSDK,
-    WalletConnectProvider
-);
-
-var address = "";
-var balance = "";
-var network = "";
-var isConnected = false;
-var signedMessage = "";
-var provider = "";
-var walletEmail = "";
+let CerthisWallet = CerthisWalletLib.init(Web3, CoinbaseWalletSDK, WalletConnectProvider);
 
 const handleConnect = async () => {
-    const get_provider = await CerthisWallet.run(
-        "1",
-        "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
-    );
-    provider = get_provider;
-    alert('here22');
-    if (get_provider) {
-        alert('here');
-        const web3 = new Web3(get_provider);
-        const accounts = await web3.eth.getAccounts();
-        const account = accounts[0];
-        //setAddress(account);
+    await handleLogout();
+    const provider = await CerthisWallet.run("1", "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161");
+    if (provider) {
+        const web3 = new Web3(provider);
+        const account = (await web3.eth.getAccounts())[0];
         const balanceInWei = await web3.eth.getBalance(account);
         const balanceInEther = web3.utils.fromWei(balanceInWei, "ether");
-        //setBalance(balanceInEther);
         const networkId = await web3.eth.net.getId();
-        //setNetwork(networkId);
-        //setIsConnected(true);
-        //setWalletEmail(localStorage.getItem("WALLET_EMAIL"));
-
-        alert(account);
+        return new Promise((resolve) => {
+            resolve(provider);
+        });
     }
 };
 
+const handleLogout = async () => {
+    await CerthisWallet.disconnect();
+}
+
+const handleSignMessage = async (provider, message) => {
+    const wallet = new ethers.providers.Web3Provider(provider);
+    const signer = await wallet.getSigner();
+    const signedMessage = await signer.signMessage(message);
+    return new Promise((resolve) => {
+        resolve(signedMessage);
+    });
+};
+
 CerthisWallet.disconnect();
-window.provider = null;
-alert('a');
-handleConnect();
+handleConnect().then((res) => {
+    handleSignMessage(res, "12345").then((res) => {
+        alert(res);
+    });
+});
