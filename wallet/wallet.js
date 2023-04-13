726535c463f5fd4e11d98ee56318c703f7d9def4
B10732009 <yuchen4667@yahoo.com.tw>
2023-04-13 15:46:18 +0800

    Connect function

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -1,6 +1,6 @@
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
 
 const Web3 = require("web3");
 let CerthisWalletLib = require("./@certhis/certhis-wallet");
@@ -10,15 +10,13 @@ let CerthisWallet = CerthisWalletLib.init(Web3, CoinbaseWalletSDK, WalletConnect
 const rpc = "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
 
 async function handleConnect() {
+    await handleDisconnect(); // force to disconnect before reconnect
     const provider = await CerthisWallet.run("1", rpc);
     if (provider) {
         const web3 = new Web3(provider);
         const account = (await web3.eth.getAccounts())[0];
-        const balanceInWei = await web3.eth.getBalance(account);
-        const balanceInEther = web3.utils.fromWei(balanceInWei, "ether");
-        const networkId = await web3.eth.net.getId();
         return new Promise((resolve) => {
-            resolve({ provider: provider, account: account });
+            resolve({ account: account });
         });
     }
 };
@@ -27,20 +25,18 @@ async function handleDisconnect() {
     await CerthisWallet.disconnect();
 }
 
-async function handleSignMessage(res, message) {
-    const wallet = new ethers.providers.Web3Provider(res.provider);
-    const signer = await wallet.getSigner();
-    const signature = await signer.signMessage(message);
-    return new Promise((resolve) => {
-        resolve({ account: res.account, signature: signature });
-    });
-};
+// async function handleSignMessage(res, message) {
+//     const wallet = new ethers.providers.Web3Provider(res.provider);
+//     const signer = await wallet.getSigner();
+//     const signature = await signer.signMessage(message);
+//     return new Promise((resolve) => {
+//         resolve({ account: res.account, signature: signature });
+//     });
+// };
 
-async function run() {
+async function connect() {
     await handleDisconnect();
-    const connectRet = await handleConnect();
-    const signMessageRet = await handleSignMessage(connectRet, "12345");
-    return signMessageRet;
+    return await handleConnect();
 }
 
-export default { run };
+export default { connect };
